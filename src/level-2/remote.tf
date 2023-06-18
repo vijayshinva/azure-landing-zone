9e51f8e636a5dc74d3fb2e051bd69f7493cc1bf8
vijayshinva <vijayshinva@gmail.com>
2023-06-19 01:32:44 +0530

    terraform_remote_state fixes

--- src/level-2/remote.tf
+++ src/level-2/remote.tf
@@ -6,7 +6,9 @@ data "terraform_remote_state" "level-1" {
   backend = "azurerm"
 
   config = {
-    container_name = "tfstate-level-1"
-    key            = "prod.level-1.tfstate"
+    container_name       = "tfstate-level-1"
+    key                  = "prod.level-1.tfstate"
+    access_key           = "${var.TFSTATE_STORAGE_ACCOUNT_KEY}"
+    storage_account_name = "${var.TFSTATE_STORAGE_ACCOUNT_NAME}"
   }
 }
--- src/level-3/remote.tf
+++ src/level-3/remote.tf
@@ -6,8 +6,10 @@ data "terraform_remote_state" "level-1" {
   backend = "azurerm"
 
   config = {
-    container_name = "tfstate-level-1"
-    key            = "prod.level-1.tfstate"
+    container_name       = "tfstate-level-1"
+    key                  = "prod.level-1.tfstate"
+    access_key           = "${var.TFSTATE_STORAGE_ACCOUNT_KEY}"
+    storage_account_name = "${var.TFSTATE_STORAGE_ACCOUNT_NAME}"
   }
 }
 
@@ -15,7 +17,9 @@ data "terraform_remote_state" "level-2" {
   backend = "azurerm"
 
   config = {
-    container_name = "tfstate-level-2"
-    key            = "prod.level-2.tfstate"
+    container_name       = "tfstate-level-2"
+    key                  = "prod.level-2.tfstate"
+    access_key           = "${var.TFSTATE_STORAGE_ACCOUNT_KEY}"
+    storage_account_name = "${var.TFSTATE_STORAGE_ACCOUNT_NAME}"
   }
 }
