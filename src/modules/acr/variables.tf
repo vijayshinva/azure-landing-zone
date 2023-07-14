8b406be9c6eded8b9c04b3b06abc3a6710b32698
Silambarasan Sivalingam <silambarasan.sivalingam@outlook.com>
2023-07-14 16:54:55 +0530

    added georeplications and quarantine

--- src/modules/acr/variables.tf
+++ src/modules/acr/variables.tf
@@ -1,32 +1,33 @@
 variable "settings" {
-  description = "acr Configuration."
+  description = "configure acr resources"
   type = object({
-    location                      = string
-    resource_group_name           = string
-    name                          = string
-    sku                           = optional(string, "Premium")
-    admin_enabled                 = optional(bool, false)
-    zone_redundancy_enabled       = optional(bool, true)
-    network_rule_bypass_option    = optional(bool, true)
-    data_endpoint_enabled         = bool
-    images_retention_enabled      = optional(bool, false)
-    images_retention_days         = number
-    trust_policy_enabled          = optional(bool, false)
+    location                   = string
+    resource_group_name        = string
+    name                       = string
+    sku                        = optional(string, "Standard")
+    admin_enabled              = optional(bool, false)
+    zone_redundancy_enabled    = optional(bool, true)
+    network_rule_bypass_option = optional(bool, true)
+    data_endpoint_enabled      = bool
 
-    georeplication_locations = optional(list(object({
+    retention_policy = object({
+      enabled = optional(bool, false)
+      days    = optional(number, 7)
+    })
+
+    trust_policy = optional(bool, true)
+
+    georeplications = list(object({
       location                  = string
       zone_redundancy_enabled   = bool
       regional_endpoint_enabled = bool
-    })))
+    }))
 
-    network_rule_set = optional(list(object({
+    network_rule_set = object({
       default_action = optional(string)
-      ip_rule = optional(list(object({
-        ip_range = string
-      })))
-      virtual_network = optional(list(object({
-        subnet_id = string
-      })))
-    })),null)
+      ip_range       = optional(list(string), [])
+      subnet_id      = optional(list(string), [])
+    })
+
   })
 }
