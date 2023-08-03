caa5e78155455e5d579e68e857fa6a52fc65c539
Silambarasan Sivalingam <silambarasan.sivalingam@outlook.com>
2023-08-03 17:36:08 +0530

    added outputs for cosmosdb

--- src/modules/cosmosdb/outputs.tf
+++ src/modules/cosmosdb/outputs.tf
@@ -13,7 +13,33 @@ output "read_endpoints" {
   description = " A list of read endpoints available for this CosmosDB account."
 }
 
-output "read_endpoints" {
-  value       = azurerm_cosmosdb_account.this.read_endpoints
+output "write_endpoints" {
+  value       = azurerm_cosmosdb_account.this.write_endpoints
   description = " A list of read endpoints available for this CosmosDB account."
 }
+
+output "primary_key" {
+  value       = azurerm_cosmosdb_account.this.primary_key
+  description = "The Primary key for the CosmosDB Account."
+}
+
+output "secondary_key" {
+  value       = azurerm_cosmosdb_account.this.secondary_key
+  description = "The Secondary key for the CosmosDB Account."
+}
+
+output "primary_readonly_key" {
+  value       = azurerm_cosmosdb_account.this.primary_readonly_key
+  description = " The Primary read-only Key for the CosmosDB Account."
+}
+
+output "secondary_readonly_key" {
+  value       = azurerm_cosmosdb_account.this.secondary_readonly_key
+  description = "The Secondary read-only key for the CosmosDB Account."
+}
+
+output "connection_strings" {
+  value       = azurerm_cosmosdb_account.this.connection_strings
+  description = "A list of connection strings available for this CosmosDB account"
+}
+
