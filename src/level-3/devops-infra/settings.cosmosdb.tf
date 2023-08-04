2c663509c36a06de66b835aec0c3def6f26c79ce
Silambarasan Sivalingam <silambarasan.sivalingam@outlook.com>
2023-08-04 13:24:14 +0530

    added geo postgres

--- src/level-3/devops-infra/settings.cosmosdb.tf
+++ src/level-3/devops-infra/settings.cosmosdb.tf
@@ -5,7 +5,6 @@ locals {
       resource_group_name = "rg-poc-tfmodule"
       name                = "cosmosdbtfmodule123"
 
-
       geo_location = [
         {
           location          = "eastus"
@@ -13,6 +12,11 @@ locals {
           }, {
           location          = "eastus2"
           failover_priority = 0
+        }
+      ]
+
+      virtual_network_rule = [{
+        id = "/subscriptions/5cb3db32-a59f-4aa5-9378-623c75ee0d8dddddddd/resourceGroups/rg-poc-tfmodule/providers/Microsoft.Network/virtualNetworks/demo-vnet1/subnets/cosmosdb"
         }
       ]
     }
